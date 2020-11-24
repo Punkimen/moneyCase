43b562da4b5d8db6820b3d8ba4bff411082f7390
max <punkimen666@gmail.com>
2020-11-24 13:24:27 +0300

    commit

--- scripts/webcam.js
+++ scripts/webcam.js
@@ -1,18 +1,20 @@
 Webcam.set({
     width: 320,
     height: 240,
-    dest_width: 320,
-    dest_height: 240,
+    dest_width: 640,
+    dest_height: 480,
     image_format: 'jpeg',
-    jpeg_quality: 90,
+    jpeg_quality: 100,
     force_flash: false,
     flip_horiz: false,
-    fps: 45
+    fps: 60
 });
 Webcam.attach('#my_camera');
 
 function take_snapshot() {
-    Webcam.snap(function(data_uri) {
-        document.getElementById('my_camera').innerHTML = '<img src="' + data_uri + '"/>';
+    Webcam.snap(function (data_uri) {
+        document.getElementById('my_result').innerHTML = '<img src="' + data_uri + '"/>';
+        $('#my_camera').hide();
+        $('.photo__wrapper-img::before').hide();
     });
 }
